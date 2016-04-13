7de841194723b7b33ff3a2b54971c076b741bb0a
izabela958 <izabela958@gmail.com>
2016-04-13 19:48:26 +0200

    opcjeczasu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        MenuForm m = new MenuForm();
+       // MenuForm m = new MenuForm();
+        opcjeCzasu o = new opcjeCzasu();
+
+
     }
 }
